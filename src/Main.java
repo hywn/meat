b5c9c09d1e323cbef2270418232cc3ff49490405
i committed this <my email>
2020-05-14 11:20:17 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	/**
+	 * runs program. takes no command-line args
+	 *
+	 * @param args command-line args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+	}
+}
